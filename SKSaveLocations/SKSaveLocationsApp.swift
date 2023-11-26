9ea0940ee3c7726606dce8f792a9345254239ee8
Serhii Kalinichenko <serhii.kalinichenko@onix-systems.com>
2023-11-26 15:44:09 +0200

    Initialisation

---
+++ SKSaveLocations/SKSaveLocationsApp.swift
@@ -0,0 +1,17 @@
+//
+//  SKSaveLocationsApp.swift
+//  SKSaveLocations
+//
+//  Created by Serhii Kalinichenko on 26.11.2023.
+//
+
+import SwiftUI
+
+@main
+struct SKSaveLocationsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
